839052fec53497c96dc7f64d489bed20cf9c6add
essam <essamelkhouly77@gmail.com>
2025-07-22 18:05:19 +0300

    some changes

--- routes/web.php
+++ routes/web.php
@@ -96,7 +96,10 @@ Route::middleware(['auth'])->group(function () {
     Route::get('notes/{id}', [NoteController::class, 'noteDetails'])->name('notes.noteDetails');
     // 📁 Item Movement
     Route::get('item-movement/{itemId?}/{warehouseId?}', [ItemController::class, 'itemMovementReport'])->name('item-movement');
-   
+    // 📁 Item Sales Report
+    Route::get('item-sales', [ItemController::class, 'itemSalesReport'])->name('item-sales');
+    // 📁 Item Purchase Report
+    Route::get('item-purchase', [ItemController::class, 'itemPurchaseReport'])->name('item-purchase');
 
 
     Route::resource('journals', JournalController::class)->names('journals');
@@ -121,8 +124,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('accounts/start-balance', [AccHeadController::class, 'startBalance'])->name('accounts.startBalance');
     // 📁 Balance Sheet
     Route::get('accounts/balance-sheet', [AccHeadController::class, 'balanceSheet'])->name('accounts.balanceSheet');
-    // 📁 Balance Sheet
-    Route::get('accounts/balance-sheet', [AccHeadController::class, 'balanceSheet'])->name('accounts.balanceSheet');
+    
     Route::resource('multi-vouchers', MultiVoucherController::class)->names('multi-vouchers');
     Route::resource('multi-journals', MultiJournalController::class)->names('multi-journals');
 
